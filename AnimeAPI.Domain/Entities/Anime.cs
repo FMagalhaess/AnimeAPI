4b538d5cb107c2808b963995406a943883ccc72c
FMagalhaess <7filipe093@gmail.com>
2025-07-17 16:38:00 -0300

    Misc: Change variable and parameters names from Portuguese to English

--- AnimeAPI.Domain/Entities/Anime.cs
+++ AnimeAPI.Domain/Entities/Anime.cs
@@ -7,30 +7,30 @@ public sealed class Anime : Entity
     #region Constructors
 
     private Anime(
-        string nome,
-        string diretor,
-        string resumo) : base(Guid.CreateVersion7())
+        string name,
+        string director,
+        string summary) : base(Guid.CreateVersion7())
     {
-        Nome = nome;
-        Diretor = diretor;
-        Resumo = resumo;
+        Name = name;
+        Director = director;
+        Summary = summary;
     }
 
     #endregion
 
     #region Properties
 
-    public string Nome { get; }
-    public string Diretor { get; }
-    public string Resumo { get; }
+    public string Name { get; }
+    public string Director { get; }
+    public string Summary { get; }
 
     #endregion
 
     #region Factories
 
-    public static Anime Create(string nome, string diretor, string resumo)
+    public static Anime Create(string name, string director, string summary)
     {
-        return new Anime(nome, diretor, resumo);
+        return new Anime(name, director, summary);
     }
 
     #endregion
