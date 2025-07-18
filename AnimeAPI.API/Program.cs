4d7de594ab475e7ab4c452fb34a3b45019327608
FMagalhaess <7filipe093@gmail.com>
2025-07-18 10:15:11 -0300

    Feat: Configure database

--- AnimeAPI.API/Program.cs
+++ AnimeAPI.API/Program.cs
@@ -1,9 +1,19 @@
+using AnimeAPI.Domain.Shared.Repositories;
+using AnimeAPI.Infrastructure.Data;
+using AnimeAPI.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IAnimeRepository, AnimeRepository>();
 
 var app = builder.Build();
 
